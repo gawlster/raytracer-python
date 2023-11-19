7fab8a2ea3d74123c0bf965df6d760c39495394e
Connor <connorgawley@yahoo.com>
2023-11-19 14:08:47 -0800

    bring back fov calculation for z value

--- camera.py
+++ camera.py
@@ -1,14 +1,13 @@
-from math import tan, radians
+from math import log, tan, radians
 from typing import Tuple
 from ray import Ray
 from vector import Vector
-from log import log
 
 
 class Camera:
     position = Vector(0, 0, 0)
     screenSize: Vector
-    fov = 60
+    fov = 120
 
     def __init__(self, resolution: Tuple[int, int]) -> None:
         self.screenSize = Vector(resolution[0], resolution[1], 0)
@@ -16,5 +15,5 @@ class Camera:
     def getDirection(self, vec: Vector) -> Ray:
         x = self.screenSize.x / 2 * ((2 * vec.x / self.screenSize.x) - 1)
         y = self.screenSize.y / 2 * ((2 * vec.y / self.screenSize.y) - 1)
-        z = -345
-        return Ray(self.position, Vector(x, -y, z).normalize())
+        z = self.screenSize.y / tan(radians(self.fov) / 2)
+        return Ray(self.position, Vector(x, -y, -z).normalize())
